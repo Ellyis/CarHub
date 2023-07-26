ce8ff83e928a6bd76057f073c4d59030aa456930
Kai Bin <kaibinyong2002@gmail.com>
2023-07-26 17:30:03 +0800

    init

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -29,7 +29,21 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
             leaveTo='opacity-0'
           >
             <Listbox.Options className='custom-filter__options'>
-              {}
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) => `relative cursor-default select-none 
+                  py-2 px-4 ${ active ? 'bg-primary-blue text-white' 
+                  : 'text-gray-900'}`}
+                >
+                  {({ selected }) => (
+                    <span>
+                      {option.title}
+                    </span>
+                  )}
+                </Listbox.Option>
+              ))}
             </Listbox.Options>
           </Transition>
         </div>
